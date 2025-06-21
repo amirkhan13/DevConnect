fb2f8d63abfacb5a379270a568819c236fda5335
amirkhan13 <amirkhan11691@gmail.com>
2025-06-21 17:35:41 +0530

    add user profile routes including get, update, upload avatar, upload cover image, and delete account

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,16 +1,28 @@
 import { Router } from "express";
 import { changePassword, loginUser, logoutUser, refershAccessToken, registerUser } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { deleteUserAccount, getCurrentUser, getUserByUsername, updateUserAvatar, updateUserCoverImage, updateUserProfile } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 
 const router = Router()
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
+router.route("/:username").get(getUserByUsername)
 
 // secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refershAccessToken)
 router.route("/change-password").patch(verifyJWT , changePassword)
+router.route("/update-profile").put(verifyJWT , updateUserProfile)
+router.route("/me").get(verifyJWT , getCurrentUser)
+router.route("/upload-avatar").put(verifyJWT ,upload.single("avatar"),updateUserAvatar);
+router.route("/upload-coverImage").put(verifyJWT ,upload.single("coverImage"),updateUserCoverImage);
+router.route("/delete").delete(verifyJWT ,deleteUserAccount);
+
+
+
 
 export default router
